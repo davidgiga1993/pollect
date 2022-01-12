1ddee6d5700d760ebcceb5963c04ffed74d839a4
david <davidgiga1993@gmail.com>
2022-01-12 12:07:36 +0100

    chore: bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pollect',
-    version='1.1.1',
+    version='1.1.2',
     author='davidgiga1993',
     author_email='david@dev-core.org',
     description='Metrics collection daemon (similar to collectd)',
