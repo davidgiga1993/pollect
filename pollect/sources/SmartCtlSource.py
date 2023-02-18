00996854a67fe5f3427e6fa008a7f3d043b47ba5
david <davidgiga1993@gmail.com>
2023-02-18 15:21:07 +0100

    fix(smart): nvme support

--- pollect/sources/SmartCtlSource.py
+++ pollect/sources/SmartCtlSource.py
@@ -1,3 +1,4 @@
+import json
 import os
 import re
 import subprocess
@@ -7,13 +8,10 @@ from pollect.sources.Source import Source
 
 
 class SmartCtlSource(Source):
-    number_matcher = re.compile('[0-9]+')
-    space_matcher = re.compile('[ \t]+')
-
     def __init__(self, config):
         super().__init__(config)
         self.devices = []
-        self.attributes = config.get('attributes')
+        self.attributes = set(config.get('attributes'))
         devices = config.get('devices')
         items = os.listdir('/dev')
         for device in devices:
@@ -22,41 +20,34 @@ class SmartCtlSource(Source):
                     self.devices.append(item)
 
     def _probe(self):
-        data = ValueSet(labels=['attribute', 'dev'])
-        required_attributes = len(self.attributes)
+        values = ValueSet(labels=['attribute', 'dev'])
         for dev in self.devices:
-            column_count = 0
-            lines = subprocess.check_output(['smartctl', '/dev/' + dev, '-a']).decode('utf-8').splitlines()
+            json_str = subprocess.check_output(['smartctl', '--json', '/dev/' + dev, '-a']).decode('utf-8')
+            data = json.loads(json_str)
 
-            # Ugly parsing - meh, it does the job (so far)
-            found_head = False
-            for line in lines:
-                line = line.strip()
+            attributes_data = data.get('ata_smart_attributes')
+            if attributes_data:
+                for attr_data in attributes_data['table']:
+                    attr_name = attr_data['name']
+                    value = attr_data['value']
+                    if self._include_attribute(attr_name):
+                        values.add(Value(int(value), label_values=[attr_name, dev]))
 
-                if found_head:
-                    columns = self.space_matcher.split(line)
-                    if len(columns) < 2:
-                        continue
-                    attribute_name = columns[1]
-                    if attribute_name not in self.attributes:
-                        continue
+            # NVME format?
+            nvme_health = data.get('nvme_smart_health_information_log')
+            if nvme_health:
+                for attr_name, value in nvme_health.items():
+                    if self._include_attribute(attr_name):
+                        if isinstance(value, list):
+                            for idx in range(len(value)):
+                                values.add(Value(int(value[idx]),
+                                                 label_values=[attr_name + '_' + str(idx), dev]))
+                            continue
 
-                    # "column_count-1" is used in instead of "-1" as the actual values might contain more
-                    # columns at the end which we need to ignore
-                    value = self.number_matcher.search(columns[column_count - 1])
-                    if not value:
-                        continue
-                    required_attributes -= 1
-                    data.add(Value(int(value.group(0)), label_values=[attribute_name, dev]))
-                    if required_attributes == 0:
-                        # We already got all the attributes - return
-                        break
-                    continue
-
-                if 'ATTRIBUTE_NAME' in line:
-                    found_head = True
-                    column_count = len(self.space_matcher.split(line))
-                    continue
+                        values.add(Value(int(value), label_values=[attr_name, dev]))
 
         # Some attributes are missing - return all matches so far
-        return data
+        return values
+
+    def _include_attribute(self, attr_name: str) -> bool:
+        return len(self.attributes) == 0 or attr_name in self.attributes
