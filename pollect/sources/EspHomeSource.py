066e0020a4fa75156b139de36bb05c075c152b83
david <davidgiga1993@gmail.com>
2024-04-30 16:18:17 +0200

    fix: esphome reconnect behavior

--- pollect/sources/EspHomeSource.py
+++ pollect/sources/EspHomeSource.py
@@ -1,5 +1,6 @@
 import asyncio
 from threading import Thread
+from time import sleep
 from typing import Optional, List, Dict
 
 import aioesphomeapi
@@ -30,23 +31,44 @@ class EspHomeSource(Source):
     def setup(self, global_conf):
         super().setup(global_conf)
         self._loop = asyncio.new_event_loop()
+        self._connect_async()
+
+    def shutdown(self):
+        super().shutdown()
+        asyncio.run_coroutine_threadsafe(self._api.disconnect(), self._loop).result(15)
+        self._loop.stop()
+
+    def _probe(self) -> Optional[ValueSet] or List[ValueSet]:
+        return self._values
+
+    def _connect_async(self):
         t = Thread(target=self._start_background_loop, args=(self._loop,), daemon=True)
         t.start()
         asyncio.run_coroutine_threadsafe(self._connect(), self._loop)
 
-    def shutdown(self):
-        super().shutdown()
+    async def _on_stop(self, expected_disconnect: bool):
+        if expected_disconnect:
+            return
 
-        asyncio.run_coroutine_threadsafe(self._api.disconnect(), self._loop)
-        self._loop.stop()
+        self.log.error('Connection lost, trying to reconnect')
+        await self._api.disconnect()
+        await self._connect()
 
     async def _connect(self):
-        await self._api.connect(login=True)
+        while True:
+            try:
+                await self._api.connect(login=True, on_stop=self._on_stop)
+                break
+            except Exception as e:
+                self.log.error(f'Could not connect {e}, retrying...')
+                sleep(10)
+
         sensors, entities = await self._api.list_entities_services()
         sensor_by_keys: Dict[int, Sensor] = dict(
             (sensor.key, Sensor(sensor.name, sensor.device_class)) for sensor in sensors)
 
         def on_event(state):
+            self.log.debug(f'Got event {state}')
             if isinstance(state, aioesphomeapi.SensorState):
                 sensor = sensor_by_keys[state.key]
                 sensor_value = state.state
@@ -59,9 +81,6 @@ class EspHomeSource(Source):
                                        name=sensor.sensor_type))
 
         self._api.subscribe_states(on_event)
-
-    def _probe(self) -> Optional[ValueSet] or List[ValueSet]:
-        return self._values
 
     @staticmethod
     def _start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
