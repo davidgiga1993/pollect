bb2825000ef5f5ed1d2add24474e128550563d64
david <davidgiga1993@gmail.com>
2025-11-01 16:53:39 +0100

    feat: ai on edge source for water meters

---
+++ pollect/sources/AiOnTheEdgeSource.py
@@ -0,0 +1,38 @@
+import requests
+from typing import Optional, List, Dict
+
+from pollect.core.ValueSet import ValueSet, Value
+from pollect.sources.Source import Source
+
+
+class AiOnTheEdgeSource(Source):
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        self._host = config['host']
+        self._port = config.get('port', 80)
+        self._value = config.get('value', 'main')
+
+    def _probe(self) -> Optional[ValueSet] | List[ValueSet]:
+        reply = requests.get(f'http://{self._host}:{self._port}/json', timeout=10)
+        data = reply.json()
+        data = data.get(self._value, {})
+
+        values = ValueSet(labels=['type'])
+        abs_value = self.to_float(data['value'])
+        if abs_value != 0:
+            # Only include value if defined, as otherwise
+            # it may indicate a counter reset to prometheus
+            values.add(Value(abs_value, ['value']))
+        values.add(Value(self.to_float(data['rate']), ['rate']))
+        values.add(Value(self.to_float(data['pre']), ['pre']))
+
+        has_error = data['error'] != 'no error'
+        values.add(Value(1 if has_error else 0, ['error']))
+        return values
+
+    def to_float(self, val: str) -> float:
+        if val == '':
+            return 0
+        return float(val)
